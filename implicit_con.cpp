76009ca995a11bd15a86d8b2ee851fdee13f85ee
Ray <97144135+rashmi-992@users.noreply.github.com>
2023-07-02 11:44:28 +0530

    implicit_con.cpp
    1.	Implicit Type Conversion Also known as ‘automatic type conversion’.
    o	Done by the compiler on its own, without any external trigger from the user.
    o	Generally takes place when in an expression more than one data type is present. In such condition type conversion (type promotion) takes place to avoid lose of data.
    o	All the data types of the variables are upgraded to the data type of the variable with largest data type.
    •	It is possible for implicit conversions to lose information, signs can be lost (when signed is implicitly converted to unsigned), and overflow can occur (when long long is implicitly converted to float).

---
+++ implicit_con.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x = 10;
+    char y = 'A';
+    cout << (x+y) << '\n';
+    float z = 5.5;
+    cout << (x+z) << '\n';
+    bool b = z;
+    cout << b << '\n';
+    return 0;
+}
